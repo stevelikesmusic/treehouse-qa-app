4c0b4305e5b0ceef8d470507157206e990e95553
Stephen Gardner <gardnerjs@gmail.com>
2016-07-23 21:08:16 -0400

    used moment for answer dates

--- app/modules/Answer.jsx
+++ app/modules/Answer.jsx
@@ -2,6 +2,7 @@ require('../styles/global.scss')
 import React from 'react'
 import Vote from './Vote'
 import config from '../../config'
+import moment from 'moment'
 
 export default React.createClass({
   contextTypes: {
@@ -22,12 +23,14 @@ export default React.createClass({
     .catch(e => console.log(`There was an error with your post: ${e.name}--${e.message}`))
   },
   render() {
+    let createdAt = `${moment(this.props.createdAt).format('MMM Do, YYYY')} at ${moment(this.props.createdAt).format('h:mm a')}`
+    let updatedAt = `${moment(this.props.updatedAt).format('MMM Do, YYYY')} at ${moment(this.props.updatedAt).format('h:mm a')}`
     return (
       <div className="answer">
         <Vote votes={this.props.upVotes} handleVote={this.handleVote} />
         <span className="answer-text">{this.props.answerText}</span>
         <div className="answer-dates">
-          Answered: {this.props.createdAt} <span>|</span> Modified: {this.props.updatedAt}
+          Answered: {createdAt} <span>|</span> Modified: {updatedAt}
         </div>
       </div>
     )
